4197497f5ae852c172e4303a119bad30c07c15b3
SahilMamedov <sahilim@code.edu.az>
2022-05-26 04:26:14 +0400

    Feat: Drag drop function

--- assest/script.js
+++ assest/script.js
@@ -9,6 +9,13 @@ function dragSart(event){
     let dataId=this.getAttribute("data-id")
     event.dataTransfer.setData("data",dataId)
 }
+function dragDrop(event){
+    let dataBox=event.dataTransfer.getData("data")
+    let dataGroup=this.getAttribute("data-id")
+    let box = document.querySelector(`.${dataBox}`)
+    if(dataBox===dataGroup){
+        this.append(box)
+    }
 
 
 
